06d2806e334d46d712cf0eae374a3eb9c21a4678
kenken509 <ariesllesis050382@gmail.com>
2023-09-04 11:16:44 +0800

    quiz

--- resources/js/Pages/Index/TestPages/StudentTakeQuiz.vue
+++ resources/js/Pages/Index/TestPages/StudentTakeQuiz.vue
@@ -4,24 +4,31 @@
         <div class="col-span-6 md:col-span-5 border-b-2 border-b-gray-600 mb-4">
             <span class="text-[20px] font-bold text-gray-500 ">Quiz Page</span>
         </div>
+        {{ userPreAnwers }} 
         <div class="">
             <span class="font-serif text-[18px] font-bold">
                 {{ quizNumber+1 }}. {{ quiz.quiz.question[quizNumber].question }}
             </span>
         </div>
-        <div class="ml-6">
-            a. {{ quiz.quiz.question[quizNumber].choices.option_a }}
+        <div class=" ml-6 gap-5">
+            <label>
+                <input type="radio" :value="quiz.quiz.question[quizNumber].choices.option_a" v-model="selectedAnswer" @change="selectAnswer" class="mr-2" @input="userAnswer(quiz.quiz.question[quizNumber].id)" />
+                {{ quiz.quiz.question[quizNumber].choices.option_a }} {{ quiz.quiz.question[quizNumber].id }}
+            </label>
         </div>
         <div class="ml-6">
-            b. {{ quiz.quiz.question[quizNumber].choices.option_b }}
+            <input type="radio" :value="quiz.quiz.question[quizNumber].choices.option_b" v-model="selectedAnswer" @change="selectAnswer" class="mr-2" @input="userAnswer(quiz.quiz.question[quizNumber].id)"/>
+             {{ quiz.quiz.question[quizNumber].choices.option_b }}
         </div>
         <div class="ml-6">
-            c. {{ quiz.quiz.question[quizNumber].choices.option_c }}
+            <input type="radio" :value="quiz.quiz.question[quizNumber].choices.option_c" v-model="selectedAnswer" @change="selectAnswer" class="mr-2" @input="userAnswer(quiz.quiz.question[quizNumber].id)"/>
+             {{ quiz.quiz.question[quizNumber].choices.option_c }}
         </div>
         <div class="ml-6">
-            d. {{ quiz.quiz.question[quizNumber].choices.option_d}}
+            <input type="radio" :value="quiz.quiz.question[quizNumber].choices.option_d" v-model="selectedAnswer" @change="selectAnswer" class="mr-2" @input="userAnswer(quiz.quiz.question[quizNumber].id)"/>
+            {{ quiz.quiz.question[quizNumber].choices.option_d}}
         </div>
-        
+        {{ selectedAnswer }}
         
         
         <div class="flex justify-between border-t-2 border-gray-600 mt-2 py-2">
@@ -32,7 +39,7 @@
             <div v-if="quizNumber+1 === quiz.quiz.question.length" class="mr-4 cursor-pointer">
                 submit
             </div>
-            <div v-else class="flex items-center gap-2 cursor-pointer " @click="handleNextClick" >
+            <div v-else class="flex items-center gap-2 cursor-pointer " @click="handleNextClick(quiz.quiz.question[quizNumber].id)" >
                 next 
                 <i class="pi pi-angle-double-right" style="font-size: 2rem"></i>
             </div>
@@ -54,8 +61,10 @@ import Pagination from '../../AdminDashboard/AdminComponents/Pagination.vue'
 
 const appUrl = '/storage/'
 const defaultImage = 'Images/default.png';
-
+const selectedAnswer = ref('');
+const userPreAnwers = ref([]);
 const user = usePage().props.user
+const quizQuestions = ref();
 const quiz = defineProps({
     quiz:Object,
     
@@ -63,14 +72,26 @@ const quiz = defineProps({
 
 const quizNumber = ref(0);
 
-const handleNextClick = ()=>
+const handleNextClick = (id)=>
 {
     
     if(quizNumber.value+1 < quiz.quiz.question.length )
     {
         quizNumber.value = quizNumber.value+1
     }
-    
+    quizQuestions.value.forEach((question)=>{
+        if(question.id === id){
+            userPreAnwers.value.push(
+                {
+                    'questionId': id,
+                    'userSelectedAnswer': selectedAnswer.value,
+                    'correct_answer': question.correct_answer,
+                }
+            )
+        }
+    })
+
+    selectedAnswer.value = null;
 }
 
 const handleBackClick = ()=>
@@ -81,6 +102,26 @@ const handleBackClick = ()=>
     }
 }
 
+onMounted(()=>{
+    quizQuestions.value = quiz.quiz.question
+})
 
-
+function userAnswer(id)
+{
+  quizQuestions.value.forEach((question)=>{
+     if(question.id === id)
+     {
+        
+        if(question.correct_answer === selectedAnswer.value)
+        {
+            console.log('tama ang sagot');
+        }
+        else{
+            console.log('mali ang sagot');
+            // console.log('user select:'+ selectedAnswer.value)
+            // console.log('correct answer:'+ question.correct_answer)
+        }
+     }
+  })
+}
 </script>
